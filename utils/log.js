bc319446f44ecf15b2f35ba6dac54cb3ef2ec55e
Le Thanh Thien <89253129+OreoZera@users.noreply.github.com>
2021-09-20 13:05:34 +0700

    Add files via upload

--- utils/log.js
+++ utils/log.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.yellow('[ ❕ Lỗi ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.red('[ ❕ Lỗi ] » ') + data);
 			break;
 		default:
 			console.log(chalk.magenta(`${option} » `) + data);
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[D-JUKIE ] » ') + data);
+			console.log(chalk.yellow('[ OREO ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[D-JUKIE ] » ') + data);
+			console.log(chalk.red('[ OREO ] » ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[D-JUKIE ] » `) + data);
+			console.log(chalk.green(`[ OREO ] » `) + data);
 			break;
 	}
 }
